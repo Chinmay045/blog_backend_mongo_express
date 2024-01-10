4dc2d2906e4e0c758f347404edb5cf7861108ac3
Chinmay <chinz.sandur07@gmail.com>
2024-01-10 11:27:21 +0530

    add all the import packages and mvc folders

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://0.0.0.0:27017/Blog')
+    .then(() => app.listen(5000))
+    .then(() => console.log("connected at port 5000"))
+    .catch((error) => console.log("Db is not connected" + error));
+
+app.use('/', (req, res, next) => {
+    res.send("Hello world");
+})
