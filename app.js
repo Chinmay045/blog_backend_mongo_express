42c7991bdb3cac3cab62f1bbf123d7213d902b45
Chinmay <chinz.sandur07@gmail.com>
2024-01-23 11:23:01 +0530

    add basic express.js file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(4000, () => console.log("connected at port 4000 "));
